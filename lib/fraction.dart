307b0b8415a47f349d66639d9dd429c0f7c6e28c
Amit Singh <amit.singh@atriina.com>
2021-09-16 14:49:50 +0530

    initial commit

---
+++ lib/fraction.dart
@@ -0,0 +1,44 @@
+// library fraction;
+
+// /// A Calculator.
+// class Calculator {
+//   /// Returns [value] plus 1.
+//   int addOne(int value) => value + 1;
+// }
+
+library flutter_custom_package;
+
+import "package:flutter/material.dart";
+
+class CustomPackageAlertBox {
+  static Future showCustomAlertBox({
+    required BuildContext context,
+    required Widget willDisplayWidget,
+  }) {
+    assert(context != null, "If context is null!!");
+    assert(willDisplayWidget != null, "If willDisplayWidget is null!!");
+    return showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            shape: const RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(Radius.circular(20)),
+            ),
+            content: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                willDisplayWidget,
+                MaterialButton(
+                  color: Colors.white70,
+                  child: const Text('Close Alert'),
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                )
+              ],
+            ),
+            elevation: 12,
+          );
+        });
+  }
+}
